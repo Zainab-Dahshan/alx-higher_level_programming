355f20989aba9e4d77f73b1bb4653f1f28e9ca40
Zainab-Dahshan <zainabeldahshan@gmail.com>
2024-01-04 14:16:27 -0800

    python import modules

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 from add_0 import add
-
-x = 1
-y = 2
-
-print("{} + {} = {}".format(x, y, add(x, y)))
+if __name__ == "__main__":
+    x = 1
+    y = 2
+    print("{:d} + {:d} = {:d}".format(x, y, add(x, y)))
