1a26565cac1d4420e14996329c6d31374a3fd763
Zainab-Dahshan <zainabeldahshan@gmail.com>
2024-01-06 03:52:49 -0800

    Everything can be imported

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from variable_load_5 import a
+# Import the variable 'a' from the module 'variable_load_5'
+if __name__ != "__main__":
+    exit()
+
+print("{:d}".format(a))
+# Print the value of 'a'. '{:d}' is a placeholder for an integer,
+# and '.format(a)' replaces the placeholder with the value of 'a'
+
