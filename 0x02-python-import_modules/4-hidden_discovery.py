7665cd0602376def29ffb0568c2e7f1d5ab02397
Zainab-Dahshan <zainabeldahshan@gmail.com>
2024-01-06 03:47:47 -0800

    Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
 import sys
-# Import sys module which provides access to some variables used
-# or maintained by Python interpreter
-
 import hidden_4 as hidden
-# Import a module named 'hidden_4' and alias it as 'hidden'
 
 if __name__ != "__main__":
-# Check if the script is being run directly or being imported
     exit()
-    # If the script is being imported, exit the script immediately
 
 for name in dir(hidden):
-# Start a loop over each attribute in the 'hidden' module
     if name[0:2] != "__":
-    # If the attribute name doesn't start with '__' (meaning it's not
-    # a special method or attribute), print its name
         print(name)
