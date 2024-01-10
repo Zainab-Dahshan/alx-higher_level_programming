bcfb188bd157929b419e2b8f34ce5d4b5e23c0be
Zainab-Dahshan <zainabeldahshan@gmail.com>
2024-01-10 13:06:39 -0800

    Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,6 @@
 def square_matrix_simple(matrix=[]):
     # Create a new matrix with the same dimensions as the input matrix
     result = [[0]*len(row) for row in matrix]
-
     # Iterate over each element in the input matrix
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
