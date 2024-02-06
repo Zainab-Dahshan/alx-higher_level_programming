229f0f2e777ae2fc3b3214bbb742bebe682b4968
Zainab-Dahshan <zainabeldahshan@gmail.com>
2024-02-06 12:10:03 -0800

    b

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+""" my len function  """
 
 
 def write_file(filename="", text=""):
-    with open(filename, "w", encoding="UTF-8") as f:
-        return f.write(text)
+    """ length of function """
+    with open(filename, 'w', encoding="utf-8") as ff:
+        rr = ff.write(text)
+    return rr
