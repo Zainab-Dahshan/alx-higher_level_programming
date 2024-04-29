3cb04b59b45df4269a2262af49ef5f96fa2fe039
Zainab-Dahshan <zainabeldahshan@gmail.com>
2024-04-29 02:51:07 -0700

    a

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
-i#!/bin/bash
-# Send JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -s -H "Content-Type: application/json" -d @"$2" "$1"
+#!/bin/bash
+# sends JSON POST as the first arg,& displays body
+curl -s "$1" -X POST -H "Content-Type: application/json" -d @"$2" 
