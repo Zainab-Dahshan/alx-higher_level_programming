b66bd10d9e8ed7c497eae73eb66bb3fef62a642d
Zainab-Dahshan <zainabeldahshan@gmail.com>
2024-04-30 02:31:16 -0700

    updates

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# header var X-HolbertonSchool-User-Id sent with value 98
-curl -s "$1" -H "X-HolbertonSchool-User-Id: 98"
+# Send a GET request to a given URL with a header variable.
+curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
